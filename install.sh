28b8b9cfd32a56404ca20161fef2f27154051af6
DrapNard <92046720+DrapNard@users.noreply.github.com>
2026-01-15 22:40:01 +0100

    Fix directory name for paru-bin build process

--- install.sh
+++ install.sh
@@ -539,7 +539,7 @@ else
   chown -R "$USERNAME:$USERNAME" "$BUILD_DIR"
 
   su - "$USERNAME" -c "cd '$BUILD_DIR' && rm -rf paru && git clone https://aur.archlinux.org/paru-bin.git"
-  su - "$USERNAME" -c "cd '$BUILD_DIR/paru' && makepkg -si --noconfirm --needed"
+  su - "$USERNAME" -c "cd '$BUILD_DIR/paru-bin' && makepkg -si --noconfirm --needed"
   PKG="$(ls -1 "$BUILD_DIR/paru"/paru-[0-9]*-x86_64.pkg.tar.* 2>/dev/null | tail -n1)"
   [[ -f "$PKG" ]] || die "paru package not built"
   pacman -U --noconfirm --needed "$PKG"
